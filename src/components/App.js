37ba976b5bdc08569c36da9c151f2b476755d685
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-08-17 01:20:37 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -3,59 +3,66 @@ import { useState, useEffect } from "react";
 
 const App = () => {
     const [todos, setTodos] = useState([]);
-    const [count, setCount] = useState(0);
-    const [inputText, setInputText] = useState('');
+    const [calculation, setCalculation] = useState("10");
+    const [textValue, setTextValue] = useState('');
+    const [inputText, setInputText] = useState([]);
 
     const handleAddTodo = () => {
         setTodos([...todos, 'New Todo']);
     };
 
     const handleIncrement = () => {
-        setCount(prevCount => prevCount + 1);
+        setCalculation(prevCount => prevCount + "0");
     };
 
     const handleInputChange = (event) => {
-        setInputText(event.target.value);
+        setTextValue(event.target.value);
     };
 
     const handleMemoSubmit = () => {
-        if (inputText.length > 5) {
-            setTodos([...todos, inputText]);
-            setInputText('');
+        if (textValue.length > 5) {
+            setInputText([...inputText, textValue]);
+            setTextValue('');
         } else {
             alert('Text should be greater than 5 characters.');
         }
     };
 
     useEffect(() => {
-        console.log('Todos:', todos);
-    }, [todos]);
+        setInputText(["HTML", "CSS", "JavaScript", "React"]);
+    }, []);
 
     return (
         <div id="main">
-            <h1>React Memo App</h1>
+            <h1>React.useMemo</h1>
+            <h2>My todos</h2>
+            <>
+                {todos.map((todo, index) => (
+                    <li id={"todo-" + index} key={index}>{todo}</li>
+                ))}
+            </>
+            <button onClick={handleAddTodo} id="add-todo-btn">Add Todo</button>
+            <hr></hr>
             <div>
-                <button onClick={handleAddTodo} id="add-todo-btn">Add Todo</button>
-                <span>{todos.length}</span>
+                <span>Count: 0<button onClick={handleIncrement} id="incr-cnt">+</button></span>
+                <h2>Expensive Calculation</h2>
+                <p id="calc">{calculation}</p>
             </div>
+            <hr></hr>
             <div>
-                <span>Count: <button onClick={handleIncrement} id="incr-cnt">{count}</button></span>
-            </div>
-            <div>
+                <h2>React.memo</h2>
                 <input
                     type="text"
                     id="skill-input"
-                    value={inputText}
+                    value={textValue}
                     onChange={handleInputChange}
                 />
                 <button id="skill-btn" onClick={handleMemoSubmit}>Submit</button>
-            </div>
-            <div>
-                <ul>
-                    {todos.map((todo, index) => (
-                        <li id={"todo-"+index} key={index}>{todo}</li>
+                <div id="#item-jumbotron">
+                    {inputText.map((todo, index) => (
+                        <li id={"todo-" + index} key={index}>{todo}</li>
                     ))}
-                </ul>
+                </div>
             </div>
         </div>
     );
